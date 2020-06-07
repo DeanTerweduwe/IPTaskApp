472fc07d5ec804d51d821268965570af02e6ca97
DeanTerweduwe <dean.terweduwe@student.ucll.be>
2020-06-07 13:23:59 +0200

    Edit and talen toegevoegd Story 6 - 7

--- src/main/java/com/example/demo/Controller/IndexController.java
+++ src/main/java/com/example/demo/Controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping("/")
     public String getIndex(){
-        return "redirect:/tasks";
+        return "index.html";
     }
 
 }
